b559765f8512822c52afe16237939187905aa20b
cmapessoal-dev <cmapessoal@gmail.com>
2026-08-26 12:37:17 -0300

    Ativa nova pagina de afastamentos

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -1,5 +1,5 @@
 (function(){
-  const VERSAO='20260826c';
+  const VERSAO='20260826d';
   window.CMA_MANUAL_VERSION=VERSAO;
   const modulos=[
     ['registro-manual','core/registro-manual.js'],
@@ -11,6 +11,7 @@
     ['tipos-contratos-tipografia','admissao/tipos-contratos-tipografia.js'],
     ['tipos-contratos-complementos','admissao/tipos-contratos-complementos.js'],
     ['ferias-conteudo','ferias/ferias-conteudo.js'],
+    ['afastamentos-conteudo','afastamentos/afastamentos-conteudo.js'],
     ['alteracoes-contratuais','alteracoes-contratuais/alteracoes-contratuais.js'],
     ['demissao-conteudo','demissao/demissao-conteudo.js'],
     ['comercio-feriados','comercio-feriados/comercio-feriados.js'],
