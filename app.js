423549a2d70d78dfe36943a69ef366221d8b6db6
cmapessoal-dev <cmapessoal@gmail.com>
2026-09-01 19:13:15 -0300

    Atualizar carregamento do Manual

--- app.js
+++ app.js
@@ -61,12 +61,12 @@ document.addEventListener('DOMContentLoaded',()=>{
 
   const core=document.createElement('script');
   core.dataset.cmaBootstrap='1';
-  core.src='core/bootstrap.js?v=20260901m';
+  core.src='core/bootstrap.js?v=20260901n';
   core.async=false;
   document.body.appendChild(core);
 
   const ms=document.createElement('script');
-  ms.src='modelos/modelos.js?v=20260901m';
+  ms.src='modelos/modelos.js?v=20260901n';
   ms.defer=true;
   document.body.appendChild(ms);
 });
